d8fd9d9356d5744eacfac9ef26d82028e9c14923
Kamil Sztefko <sztefkokamil@gmail.com>
2019-07-23 11:45:46 +0200

    Add toUpper
    Added inputToUpper variable.
    Added toUpper method with dispatch(toUpper).
    Added form with toUpper call on submit.
    Added input & button into form.

--- src/components/LeftPanel/LeftPanel.vue
+++ src/components/LeftPanel/LeftPanel.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="left-panel__container">
     <button class="left-panel__clickCounterIncrement" @click="$store.commit('incrementClickCounter')">increment</button>
+    <form class="left-panel__toUpper-form" @submit.prevent="toUpper">
+      <input class="toUpper-input" type="text" v-model="inputToUpper">
+      <button class="left-panel__toUpperBtn">transform to Upper</button>
+    </form>
   </div>
 </template>
 
@@ -8,6 +12,17 @@
 
 export default {
   name: 'LeftPanel',
+  data() {
+    return {
+      inputToUpper: ''
+    }
+  },
+  methods: {
+    toUpper(){
+      this.$store.dispatch('toUpper', this.inputToUpper);
+      this.inputToUpper = '';
+    }
+  }
 }
 </script>
 
