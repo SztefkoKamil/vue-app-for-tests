616a1d4a0528c65c6ba0da8c5244c20d04c73a73
Kamil Sztefko <sztefkokamil@gmail.com>
2019-07-23 16:51:26 +0200

    Add tests for toReverse in RightPanel

--- src/components/RightPanel/RightPanel.test.js
+++ src/components/RightPanel/RightPanel.test.js
@@ -2,20 +2,24 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import RightPanel from './RightPanel.vue'
 
-describe('TopBoard.vue - tests', () => {
+describe('RightPanel.vue - tests', () => {
 
   const localVue = createLocalVue()
   localVue.use(Vuex)
 
-  let store, mutations
+  let store, mutations, actions
   
   beforeEach(() => {
     mutations = {
       decrementClickCounter: jest.fn(() => {})
+    },
+    actions = {
+      toReverse: jest.fn(() => {})
     }
-    store = new Vuex.Store({ mutations })
+    store = new Vuex.Store({ mutations, actions })
   })
 
+  // counterClick
   it('should have button.right-panel__clickCounterDecrement', () => {
     const wrapper = shallowMount(RightPanel,)
 
@@ -24,12 +28,46 @@ describe('TopBoard.vue - tests', () => {
     expect(button).toBeTruthy()
   })
 
-  it('btn click should call DecrementClickCounter mutation', () => {
+  it('btn click should call decrementClickCounter mutation', () => {
     const wrapper = shallowMount(RightPanel, { store, localVue })
 
     wrapper.find('button.right-panel__clickCounterDecrement').trigger('click')
     
     expect(mutations.decrementClickCounter).toHaveBeenCalled()
+  })
+
+
+  //toReverse
+  it('should have button.right-panel__toReverseBtn', () => {
+    const wrapper = shallowMount(RightPanel)
+
+    const button = wrapper.find('button.right-panel__toReverseBtn')
+    
+    expect(button).toBeTruthy()
+  })
+
+  it('input.toReverse-input output should be stored in inputToUpper', () => {
+    const wrapper = shallowMount(RightPanel)
+
+    wrapper.find('input.toReverse-input').setValue('hello')
+
+    expect(wrapper.vm.inputToReverse).toBe('hello')
+  })
+
+  it('form.right-panel__toReverse-form submit should call toReverse action', () => {
+    const wrapper = shallowMount(RightPanel, { store, localVue })
+
+    wrapper.find('form.right-panel__toReverse-form').trigger('submit')
+    
+    expect(actions.toReverse).toHaveBeenCalled()
+  })
+
+  it('form.right-panel__toReverse-form submit should clear inputToUpper', () => {
+    const wrapper = shallowMount(RightPanel, { store, localVue })
+
+    wrapper.find('form.right-panel__toReverse-form').trigger('submit')
+    
+    expect(wrapper.vm.inputToReverse).toBe('')
   })
 
 })
