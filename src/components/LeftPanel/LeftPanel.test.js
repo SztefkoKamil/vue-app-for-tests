bc6d097842e39feecdf3ad176eec5f7478af8431
Kamil Sztefko <sztefkokamil@gmail.com>
2019-07-23 11:46:11 +0200

    Add tests for LeftPanel.vue

--- src/components/LeftPanel/LeftPanel.test.js
+++ src/components/LeftPanel/LeftPanel.test.js
@@ -2,22 +2,26 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import LeftPanel from './LeftPanel.vue'
 
-describe('TopBoard.vue - tests', () => {
+describe('leftPanel.vue - tests', () => {
 
   const localVue = createLocalVue()
   localVue.use(Vuex)
 
-  let store, mutations
+  let store, mutations, actions
   
   beforeEach(() => {
     mutations = {
       incrementClickCounter: jest.fn(() => {})
     }
-    store = new Vuex.Store({ mutations })
+    actions = {
+      toUpper: jest.fn(() => {})
+    }
+    store = new Vuex.Store({ mutations, actions })
   })
 
+    // counter tests
   it('should have button.left-panel__clickCounterIncrement', () => {
-    const wrapper = shallowMount(LeftPanel,)
+    const wrapper = shallowMount(LeftPanel)
 
     const button = wrapper.find('button.left-panel__clickCounterIncrement')
     
@@ -30,6 +34,39 @@ describe('TopBoard.vue - tests', () => {
     wrapper.find('button.left-panel__clickCounterIncrement').trigger('click')
     
     expect(mutations.incrementClickCounter).toHaveBeenCalled()
+  })
+
+    // toUpper tests
+  it('should have button.left-panel__toUpperBtn', () => {
+    const wrapper = shallowMount(LeftPanel)
+
+    const button = wrapper.find('button.left-panel__toUpperBtn')
+    
+    expect(button).toBeTruthy()
+  })
+
+  it('input.toUpper-input output shpuld be stored in inputToUpper', () => {
+    const wrapper = shallowMount(LeftPanel)
+
+    wrapper.find('input.toUpper-input').setValue('ok')
+
+    expect(wrapper.vm.inputToUpper).toBe('ok')
+  })
+
+  it('form.left-panel__toUpper-form submit should call toUpper action', () => {
+    const wrapper = shallowMount(LeftPanel, { store, localVue })
+
+    wrapper.find('form.left-panel__toUpper-form').trigger('submit')
+    
+    expect(actions.toUpper).toHaveBeenCalled()
+  })
+
+  it('form.left-panel__toUpper-form submit should clear inputToUpper', () => {
+    const wrapper = shallowMount(LeftPanel, { store, localVue })
+
+    wrapper.find('form.left-panel__toUpper-form').trigger('submit')
+    
+    expect(wrapper.vm.inputToUpper).toBe('')
   })
 
 })
