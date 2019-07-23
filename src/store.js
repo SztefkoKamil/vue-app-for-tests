74dcbbc01aceb8f7a04af41c647e93954f3882f5
Kamil Sztefko <sztefkokamil@gmail.com>
2019-07-23 11:42:20 +0200

    Add state & methods for toUpper

--- src/store.js
+++ src/store.js
@@ -5,11 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    clickCounter: 0
+    clickCounter: 0,
+    toUpper: ''
   },
   getters: {
     getClickCounter(state) {
       return state.clickCounter
+    },
+    getToUpper(state){
+      return state.toUpper
     }
   },
   mutations: {
@@ -18,9 +22,14 @@ export default new Vuex.Store({
     },
     decrementClickCounter(state) {
       state.clickCounter--
+    },
+    toUpper(state, data){
+      state.toUpper = data;
     }
   },
   actions: {
-
+    toUpper(context, data){
+      context.commit('toUpper', data.toUpperCase());
+    }
   }
 })
